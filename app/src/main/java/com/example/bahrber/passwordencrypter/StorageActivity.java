45a560ad825f8e7f7cf1bf896e275ed5be8a866b
GregglesHere <gfernandes7167@gmail.com>
2018-12-11 14:50:29 -0600

    Saving Data almost done. Put what you want saved in array

--- app/src/main/java/com/example/bahrber/passwordencrypter/StorageActivity.java
+++ app/src/main/java/com/example/bahrber/passwordencrypter/StorageActivity.java
@@ -1,12 +1,53 @@
 package com.example.bahrber.passwordencrypter;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class StorageActivity extends AppCompatActivity {
+    private ArrayList<String> passwordList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.storage);
+
+        loadData();
+
+        Button buttonSave = findViewById(R.id.button_save);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(passwordList);
+        editor.putString("task list", json);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        passwordList = gson.fromJson(json, type);
+
+        if (passwordList == null) {
+            passwordList = new ArrayList<>();
+        }
     }
 }
